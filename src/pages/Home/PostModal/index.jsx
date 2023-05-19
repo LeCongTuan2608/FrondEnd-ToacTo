365d879cb3fed08cbcbdfd2a94c366fbed289eae
Tuấn Lê <tuancnttk61@gmail.com>
2023-05-19 13:53:39 +0700

    update

--- src/pages/Home/PostModal/index.jsx
+++ src/pages/Home/PostModal/index.jsx
@@ -1,7 +1,10 @@
-import { UserOutlined } from '@ant-design/icons';
-import { Avatar, Button, Modal, Select } from 'antd';
+import { PlusOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Divider, Modal, Select, Space, Upload } from 'antd';
 import img_avt from '../../../images/avatar.png';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useRef } from 'react';
+import classNames from 'classnames/bind';
+import styles from './PostModal.module.scss';
+const cn = classNames.bind(styles);
 PostModal.propTypes = {};
 const options = [
    {
@@ -17,24 +20,72 @@ const options = [
       label: 'Private',
    },
 ];
+const getBase64 = (file) =>
+   new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+   });
 function PostModal(props) {
    const { modalOpen, setModalOpen } = props;
    const [dis, setDis] = useState(true);
    const [loadings, setLoadings] = useState(false);
-   const handleChange = (value) => {
-      console.log(`selected ${value}`);
-   };
-   const handleOnchange = (e) => {
-      if (e.target.innerText === '') {
-         setDis(!dis);
+   const [audience, setAudience] = useState('public');
+   const content = useRef();
+   //============= Upload ========================
+   const [previewOpen, setPreviewOpen] = useState(false);
+   const [previewImage, setPreviewImage] = useState('');
+   const [previewTitle, setPreviewTitle] = useState('');
+   const [fileList, setFileList] = useState([]);
+   const handleCancel = () => setPreviewOpen(false);
+   const handlePreview = async (file) => {
+      if (!file.url && !file.preview) {
+         file.preview = await getBase64(file.originFileObj);
       }
+      setPreviewImage(file.url || file.preview);
+      setPreviewOpen(true);
+      setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
    };
-   const handleCreatePost = (index) => {
-      setLoadings(true);
+   const handleChange = ({ fileList: newFileList }) => {
+      setFileList(newFileList);
+      setDis(false);
+   };
+   const dummyRequest = async ({ file, onSuccess }) => {
       setTimeout(() => {
-         setLoadings(false);
-         setModalOpen(false);
-      }, 3000);
+         onSuccess('ok');
+      }, 0);
+   };
+
+   //==================================================
+   const handleChangeAudience = (value) => {
+      setAudience(value);
+   };
+   const handleInputContent = (e) => {
+      content.current.innerText === '' ? setDis(true) : setDis(false);
+   };
+   const handleCreatePost = async () => {
+      try {
+         console.log({
+            content: content.current.innerText,
+            file: fileList,
+            userName: localStorage.getItem('userName'),
+            audience: audience,
+         });
+         setLoadings(true);
+         setTimeout(() => {
+            setLoadings(false);
+            setModalOpen(false);
+            //clear modal
+            content.current.innerText = '';
+            setFileList([]);
+            setPreviewOpen(false);
+            setDis(!dis);
+            //
+         }, 3000);
+      } catch (error) {
+         console.log('error:', error);
+      }
    };
    return (
       <Modal
@@ -66,22 +117,51 @@ function PostModal(props) {
                   style={{
                      width: 120,
                   }}
-                  onChange={handleChange}
+                  onChange={handleChangeAudience}
                   options={options}
                />
             </div>
          </div>
-         <div
-            onKeyDown={handleOnchange}
-            contentEditable
-            style={{
-               minHeight: 200,
-               outline: 'none',
-               fontSize: 18,
-               margin: '20px 0',
-               maxHeight: 450,
-               overflowY: 'scroll',
-            }}></div>
+         <div className={cn('content-wrap')}>
+            {(!content.current || content.current.innerText === '') && (
+               <span>What are you thinking?</span>
+            )}
+            <div
+               onInput={handleInputContent}
+               ref={content}
+               contentEditable
+               className={cn('content-edit')}></div>
+         </div>
+         <Divider />
+         <div className={cn('box-upload')}>
+            <Upload
+               accept="file"
+               listType="picture-card"
+               fileList={fileList}
+               multiple={true}
+               onPreview={handlePreview}
+               customRequest={dummyRequest}
+               onChange={handleChange}>
+               <div>
+                  <PlusOutlined />
+                  <div
+                     style={{
+                        marginTop: 8,
+                     }}>
+                     Upload
+                  </div>
+               </div>
+            </Upload>
+            <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+               <img
+                  alt="example"
+                  style={{
+                     width: '100%',
+                  }}
+                  src={previewImage}
+               />
+            </Modal>
+         </div>
          <div>
             <Button
                type="primary"
