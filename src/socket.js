2fdef73657e3bcbc084c444824c3ccb242650a3f
Tuấn Lê <tuancnttk61@gmail.com>
2023-06-27 22:15:50 +0700

    update

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
-export const host = 'http://localhost:4000';
+// export const host = 'http://localhost:4000';
+export const host = 'https://web-social-2c4s.onrender.com';
 const socket = io(host, {
    autoConnect: false,
    // reconnection: false,
